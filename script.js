fc8dffef91c5301f9df59a3d0b1c2828fe1f7345
Arghya <arghya2801@gmail.com>
2024-10-24 19:41:52 +0530

    Fixed the issue in gameBoard() not passing the argument in check_victory

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ function transpose(board) {
 let winner = 0;
 
 function check_victory(board) {
+    console.log("Checking winner");
     // Row victories
     if (
         (board[0][0] === 1 && board[0][1] === 1 && board[0][2] === 1) || // X row victory
@@ -62,7 +63,7 @@ function check_victory(board) {
 
 function update_X(x, y) {
     try {
-        if (x > 2 || x < 0 || y > 2 || y < 0) {
+        if (x > 3 || x < 1 || y > 3 || y < 1) {
             throw new Error("Values of x and y must be between 0 and 3.");
         }
         if (board[x - 1][y - 1] != 0) {
@@ -70,7 +71,7 @@ function update_X(x, y) {
         }
 
         board[x - 1][y - 1] = 1;
-        check_victory(board);
+        // check_victory(board);
 
     } catch (error) {
         console.error(error.message);
@@ -80,7 +81,7 @@ function update_X(x, y) {
 
 function update_O(x, y) {
     try {
-        if (x > 2 || x < 0 || y > 2 || y < 0) {
+        if (x > 3 || x < 1 || y > 3 || y < 1) {
             throw new Error("Values of x and y must be between 0 and 3.");
         }
         if (board[x - 1][y - 1] != 0) {
@@ -88,7 +89,7 @@ function update_O(x, y) {
         }
 
         board[x - 1][y - 1] = -1;
-        check_victory(board);
+        // check_victory(board);
 
     } catch (error) {
         console.error(error.message);
@@ -129,27 +130,35 @@ function displayBoard() {
             }
         }
         console.log(row.join(' | '));
-        if (i < 2) {
-            console.log('---------');
-        }
+        // if (i < 2) {
+        //     console.log('---------');
+        // }
     }
 }
 
 function gameBoard() {
     while (winner === 0 && board.flat().includes(0)) {
-        // X move
         console.log("Input X move: ");
         let x_coordinate = coordinate();
-        update_X(x_coordinate[0], x_coordinate[1]);
-        displayBoard();
-        // O move
+        if (x_coordinate) { 
+            update_X(x_coordinate[0], x_coordinate[1]);
+            displayBoard();
+            check_victory(board);
+        } else {
+            continue;
+        }
+
+        if (winner !== 0) break; 
+
         console.log("Input O move: ");
         let o_coordinate = coordinate();
-        update_O(o_coordinate[0], o_coordinate[1]);
-        displayBoard();
-
-        check_victory();
-        // if (winner === 1 || winner === -1) {return;}
+        if (o_coordinate) {
+            update_O(o_coordinate[0], o_coordinate[1]);
+            displayBoard();
+            check_victory(board);
+        } else {
+            continue;
+        }
     }
 }
 
